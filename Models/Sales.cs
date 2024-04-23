fb9826fe40988527fd9be9c6fd7e7c767526c928
Marcos Leite <m.abner@live.co.uk>
2024-04-23 01:20:45 -0300

    serializing the object - indented formatting

--- Models/Sales.cs
+++ Models/Sales.cs
@@ -7,6 +7,15 @@ namespace Nuget_Serializar_e_Atributos_CSharp.Models
 {
     public class Sales
     {
+
+        public Sales(int id, string product, decimal price)
+        {
+            Id = id;
+            Product = product;
+            Price = price;
+        }
+
+
         public int Id { get; set; }
         public string Product { get; set; }
         public decimal Price { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Nuget_Serializar_e_Atributos_CSharp.Models;
+using Newtonsoft.Json;
+
+Sales s1 = new Sales(1, "Coral Sand N1", 180.00M);
+
+// serializing the object
+string serialized = JsonConvert.SerializeObject(s1, Formatting.Indented);
+
+Console.WriteLine(serialized);
+
