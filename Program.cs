4c75f0bcd5b3c6d4fcbf544596cd9192ddd58d14
Marcos Leite <m.abner@live.co.uk>
2024-04-23 01:59:34 -0300

    serializing a collection (list)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿using Newtonsoft.Json;
 using Nuget_Serializar_e_Atributos_CSharp.Models;
 
-Sales s1 = new Sales(1, "Coral Sand N1", 180.00M);
+// object collection
+List<Sales> salesList = new List<Sales>();
 
-// serializing the object
-string serialized = JsonConvert.SerializeObject(s1, Formatting.Indented);
+Sales s1 = new Sales(1, "Coral Sand N1", 180.00M);
+Sales s2 = new Sales(2, "Samsung Evo 890 Pro 1TB", 1148.00M);
+
+
+salesList.Add(s1);
+salesList.Add(s2);
+
+
+// serializing the list of object
+string serialized = JsonConvert.SerializeObject(salesList, Formatting.Indented);
 
 File.WriteAllText("Files/sales.json", serialized);
 
