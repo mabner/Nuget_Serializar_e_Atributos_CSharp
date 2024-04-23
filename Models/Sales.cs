84ee3ffacf735cd7f95bc2d2d8f99b0385726ba2
Marcos Leite <m.abner@live.co.uk>
2024-04-23 02:22:35 -0300

    adds datetime.now to the sales

--- Models/Sales.cs
+++ Models/Sales.cs
@@ -7,17 +7,17 @@ namespace Nuget_Serializar_e_Atributos_CSharp.Models
 {
     public class Sales
     {
-
-        public Sales(int id, string product, decimal price)
+        public Sales(int id, string product, decimal price, DateTime salesDate)
         {
             Id = id;
             Product = product;
             Price = price;
+            SalesDate = salesDate;
         }
-
 
         public int Id { get; set; }
         public string Product { get; set; }
         public decimal Price { get; set; }
+        public DateTime SalesDate { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿using Newtonsoft.Json;
 using Nuget_Serializar_e_Atributos_CSharp.Models;
 
+DateTime dateNow = DateTime.Now;
+
 // object collection
 List<Sales> salesList = new List<Sales>();
 
-Sales s1 = new Sales(1, "Coral Sand N1", 180.00M);
-Sales s2 = new Sales(2, "Samsung Evo 890 Pro 1TB", 1148.00M);
-
+Sales s1 = new Sales(1, "Coral Sand N1", 180.00M, dateNow);
+Sales s2 = new Sales(2, "Samsung Evo 890 Pro 1TB", 1148.00M, dateNow);
 
 salesList.Add(s1);
 salesList.Add(s2);
-
 
 // serializing the list of object
 string serialized = JsonConvert.SerializeObject(salesList, Formatting.Indented);
@@ -18,4 +18,3 @@ string serialized = JsonConvert.SerializeObject(salesList, Formatting.Indented);
 File.WriteAllText("Files/sales.json", serialized);
 
 Console.WriteLine(serialized);
-
