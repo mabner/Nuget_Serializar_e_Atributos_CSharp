52f5942d89c8478e255e10db197224ebdeecfab2
Marcos Leite <m.abner@live.co.uk>
2024-04-23 01:38:11 -0300

    saving the serialized object to a json file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿using Nuget_Serializar_e_Atributos_CSharp.Models;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using Nuget_Serializar_e_Atributos_CSharp.Models;
 
 Sales s1 = new Sales(1, "Coral Sand N1", 180.00M);
 
 // serializing the object
 string serialized = JsonConvert.SerializeObject(s1, Formatting.Indented);
+
+File.WriteAllText("Files/sales.json", serialized);
 
 Console.WriteLine(serialized);
 
