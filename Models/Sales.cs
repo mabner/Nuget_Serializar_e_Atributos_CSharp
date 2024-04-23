2eb4c6ddbdfa7902de394ebe5320608e638456e5
Marcos Leite <m.abner@live.co.uk>
2024-04-23 00:28:11 -0300

    Adds the sales model class

---
+++ Models/Sales.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nuget_Serializar_e_Atributos_CSharp.Models
+{
+    public class Sales
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+    }
+}
