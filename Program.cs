b80c1027d2f56939def545ddae1b4b33ac0e678e
Marcos Leite <m.abner@live.co.uk>
2024-04-23 03:37:39 -0300

    deserializing the json file into an object

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
-﻿using Newtonsoft.Json;
+﻿using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
 using Nuget_Serializar_e_Atributos_CSharp.Models;
 
-DateTime dateNow = DateTime.Now;
+// -- Deserializing --
+string fileContent = File.ReadAllText("Files/sales.json");
 
-// object collection
-List<Sales> salesList = new List<Sales>();
+List<Sales> salesList = JsonConvert.DeserializeObject<List<Sales>>(fileContent);
 
-Sales s1 = new Sales(1, "Coral Sand N1", 180.00M, dateNow);
-Sales s2 = new Sales(2, "Samsung Evo 890 Pro 1TB", 1148.00M, dateNow);
+foreach (Sales sales in salesList)
+{
+    Console.WriteLine(
+        $"ID: {sales.Id}, Item: {sales.Product}"
+            + $"Price: {sales.Price}. Date: {sales.SalesDate.ToString("dd/MM/yyyy HH:mm")}"
+    );
+}
 
-salesList.Add(s1);
-salesList.Add(s2);
 
-// serializing the list of object
-string serialized = JsonConvert.SerializeObject(salesList, Formatting.Indented);
 
-File.WriteAllText("Files/sales.json", serialized);
 
-Console.WriteLine(serialized);
+
+
+// -- Serializing --
+// DateTime dateNow = DateTime.Now;
+
+// // object collection
+// List<Sales> salesList = new List<Sales>();
+
+// Sales s1 = new Sales(1, "Coral Sand N1", 180.00M, dateNow);
+// Sales s2 = new Sales(2, "Samsung Evo 890 Pro 1TB", 1148.00M, dateNow);
+
+// salesList.Add(s1);
+// salesList.Add(s2);
+
+// // serializing the list of object
+// string serialized = JsonConvert.SerializeObject(salesList, Formatting.Indented);
+
+// File.WriteAllText("Files/sales.json", serialized);
+
+// Console.WriteLine(serialized);
